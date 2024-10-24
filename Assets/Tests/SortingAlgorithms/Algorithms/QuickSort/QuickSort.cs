edc06dc3a6d0e64325695cfa9125426595c01e85
Joey Rieg <klein.winz@web.de>
2024-10-24 17:07:32 +0200

    Implement QuickSort

--- Assets/Tests/SortingAlgorithms/Algorithms/QuickSort/QuickSort.cs
+++ Assets/Tests/SortingAlgorithms/Algorithms/QuickSort/QuickSort.cs
@@ -7,7 +7,49 @@ namespace SortingAlgorithms
   {
     public override void Sort(int[] numbers)
     {
+      // define pivot element as last element
+      // partition such that left side is smaller than pivot
+      // and right side is bigger
+      // recurse until sorted
+      Sort(numbers, 0, numbers.Length - 1);
+    }
 
+    private void Sort(int[] array, int start, int end)
+    {
+      if (start >= end)
+        return;
+
+      int pivot = Partition(array, start, end);
+      Sort(array, start, pivot - 1);
+      Sort(array, pivot + 1, end);
+    }
+
+    private int Partition(int[] array, int start, int end)
+    {
+      Debug.Log($"Partition with {start} and {end}");
+      int pivot = array[end];
+      
+      int i = start - 1;
+      int j = start;
+
+      while (j < end)
+      {
+        if (array[j] < pivot)
+        {
+          ++i;
+          int temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
+        }
+
+        ++j;
+      }
+
+      int t = array[++i];
+      array[i] = array[end];
+      array[end] = t;
+
+      return i;
     }
   }
 }
